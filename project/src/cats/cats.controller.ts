69f2db013f28db6b1ab8dfbaba73d30810c9b745
kangjs3779 <kangjs3779@gmail.com>
2025-01-13 21:41:49 +0900

    섹션4 완료

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -8,21 +8,25 @@ import {
   Patch,
   HttpException,
   ParseIntPipe,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { PositiveIntPipe } from 'src/common/pipes/positiveint.pipe';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat(): string {
-    throw new HttpException('api is broken', 401);
+    console.log('getAllCat');
     return 'All cat';
   }
 
   @Get(':id')
-  getOneCat(@Param('id', ParseIntPipe) param: number) {
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
     console.log(typeof param);
     return `one cat ${param}`;
   }
